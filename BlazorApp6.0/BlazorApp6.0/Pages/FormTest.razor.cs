d462934482f4cc5b12acf7381f02179d8eaeaea3
Nikolaos Fragos <nifragos@gmail.com>
2022-04-28 12:03:43 +0300

    added example

--- BlazorApp6.0/BlazorApp6.0/Pages/FormTest.razor.cs
+++ BlazorApp6.0/BlazorApp6.0/Pages/FormTest.razor.cs
@@ -3,14 +3,16 @@
 public partial class FormTest 
 {
     public CustomValidator customValidator;
-    public UserVm userVm { get; set; } = new ();
+    public UserVm UserVm { get; set; } = new ();
     
     private void HandleValidSubmit()
     {
+        customValidator.ClearErrors();
+        
         customValidator?.DisplayErrors(new Dictionary<string, List<string>>()
         {
-            { nameof(userVm.Username), new List<string>() { "Username error", "Username second error" } },
-            {  nameof(userVm.AddressVm.Street), new List<string>() { "Street Address error", " second street address error" } }
+            { nameof(UserVm.Username), new List<string>() { "Username error", "Username second error" } },
+            {  nameof(UserVm.AddressVm.Street), new List<string>() { "Street Address error", " second street address error" } }
         });
     }
 }
--- BlazorApp6.0/BlazorApp6.0/UserVm.cs
+++ BlazorApp6.0/BlazorApp6.0/UserVm.cs
@@ -6,7 +6,6 @@ public class UserVm
 {
     public string Username { get; set; }
 
-    [ValidateComplexType] 
     public AddressVm AddressVm { get; set; } = new AddressVm();
 
 }
