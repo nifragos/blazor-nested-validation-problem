18bb573a6799c1873ce2a1180d8df0747c9932a6
Nikolaos Fragos <nifragos@gmail.com>
2022-04-28 11:29:52 +0300

    second commit

---
+++ BlazorApp6.0/BlazorApp6.0/Pages/FormTest.razor.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorApp6._0.Pages;
+
+public partial class FormTest 
+{
+    public CustomValidator customValidator;
+    public UserVm UserVm { get; set; } = new ();
+    
+    private void HandleValidSubmit()
+    {
+        customValidator?.DisplayErrors(new Dictionary<string, List<string>>()
+        {
+            { nameof(UserVm.Username), new List<string>() { "Username error", "Username second error" } },
+            {  nameof(UserVm.AddressVm.Street), new List<string>() { "Street Address error", " second street address error" } }
+        });
+    }
+}
---
+++ BlazorApp6.0/BlazorApp6.0/UserVm.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp6._0;
+
+public class UserVm
+{
+    public string Username { get; set; }
+
+    [ValidateComplexType] 
+    public AddressVm AddressVm { get; set; } = new AddressVm();
+
+}
+
+public class AddressVm
+{
+    public string Street { get; set; }
+}
